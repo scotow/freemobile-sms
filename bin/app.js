064b9c85956f16fdefe64a0b3340cb4aec090f6b
Benjamin <benjamin-lopez@hotmail.fr>
2017-09-16 20:59:22 +0200

    Remove postinstall script and add configuration command

--- bin/app.js
+++ bin/app.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
-const api = require('../lib/freesmsapi.js');
+const fs = require('fs');
+const credentialsPath = __dirname ++ '/../credentials.json');
+if(fs.existsSync(credentialsPath) || (process.argv[2] === '--config')) {
+	require('../lib/config.js')();
+} else {
+	const api = require('../lib/freesmsapi.js');
+	const credentials = require('../credentials.json');
 
-credentials = require('../credentials.json');
-if(!credentials.user) throw ReferenceError('Invalid user credential.');
-if(!credentials.password) throw ReferenceError('Invalid password credential.');
+	if(!credentials.user) throw ReferenceError('Invalid user credential.');
+	if(!credentials.password) throw ReferenceError('Invalid password credential.');
 
-const message = process.argv.slice(2).join(' ');
-if(!message) throw SyntaxError('Invalid message.');
+	const message = process.argv.slice(2).join(' ');
+	if(!message) throw SyntaxError('Invalid message.');
 
-api.send(message, credentials)
-.catch(statusCode => {
-	process.exit(statusCode);
-});
+	api.send(message, credentials)
+	.catch(statusCode => {
+		process.exit(statusCode);
+	});
+}
