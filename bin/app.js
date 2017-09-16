f4425721a8d9e0311286e0d5414c1d1f3bb49c04
Benjamin <benjamin-lopez@hotmail.fr>
2017-09-16 18:32:05 +0200

    Add API and command tools

---
+++ bin/app.js
@@ -0,0 +1,13 @@
+const api = require('../lib/freesmsapi.js');
+
+credentials = require('../credentials.json');
+if(!credentials.user) throw ReferenceError('Invalid user credential.');
+if(!credentials.password) throw ReferenceError('Invalid password credential.');
+
+const message = process.argv.slice(2).join(' ');
+if(!message) throw SyntaxError('Invalid message.');
+
+api.send(message, credentials)
+.catch(statusCode => {
+	process.exit(statusCode);
+});
