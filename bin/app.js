953aea105a3b45449af8fa9e648eec2ab7d188ba
Scotow <contact@scotow.com>
2017-10-27 18:41:35 +0200

    Move exeception from bin command to api lib.

--- bin/app.js
+++ bin/app.js
@@ -1,24 +1,32 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const fs = require('fs');
-const credentialsPath = __dirname + '/../credentials.json';
+
+const credentialsPath = path.join(__dirname, '..', 'credentials.json');
+
 if(!fs.existsSync(credentialsPath) || (process.argv[2] === '--config')) {
-	require('../lib/config.js')();
+	require('../lib/config.js')()
+	.then(sendMessage);
 } else {
+	sendMessage();
+}
+
+function sendMessage() {
 	const api = require('../lib/freesmsapi.js');
 	const credentials = require('../credentials.json');
 
-	if(!credentials.user) throw ReferenceError('Invalid user credential.');
-	if(!credentials.password) throw ReferenceError('Invalid password credential.');
-
-	const message = process.argv.slice(2).join(' ');
-	if(!message) throw SyntaxError('Invalid message.');
-
-	api.send(message, credentials)
+	api.send(process.argv.slice(2).join(' '), credentials)
 	.then(() => {
 		process.exit(0);
 	})
-	.catch(statusCode => {
-		process.exit(statusCode);
+	.catch((error) => {
+		if(typeof error === 'number') {
+			console.error(`The FreeMobile API return the following HTTP status code: ${error}.`);
+			process.exit(error);
+		} else {
+			console.error(error.message);
+			process.exit(1);
+		}
 	});
 }
